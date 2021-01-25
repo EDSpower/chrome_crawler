0c2572d63f9c2353939be5b95a431ed42105b542
EDSpower <766782971@qq.com>
2021-01-26 00:06:54 +0800

    add_2021/1/26

--- js/getimgs.js
+++ js/getimgs.js
@@ -1,11 +1,13 @@
 console.log("i am down");
 
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-  if (request.type == "getImg")
+  if (request.type == "getImg") {
     //判断是否为要处理的消息
     sendResponse({
       list: getImgListUrl(),
     });
+  }
+
 });
 
 // 获取当前web页面的所有图片信息
--- main.js
+++ main.js
@@ -1,38 +1,56 @@
 console.log("i am main");
 const btn = document.querySelector(".btn");
 
-// 扩展向content_script通讯
-btn.addEventListener("click", () => {
-  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-    chrome.tabs.sendMessage(
-      tabs[0].id,
-      { type: "getImg" },
-      async function (response) {
-        if (response && response.list) {
-          markImgDom(response.list)
-          for (const item of response.list) {
-            await delayFn(500)
-            downloadImg(item)
-            console.log(response.list);
-          }
-        }
+let imgUrlList = []
+
+// 初始化
+chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+  chrome.tabs.sendMessage(
+    tabs[0].id,
+    { type: "getImg" },
+    function (response) {
+      if (response && response.list) {
+        const descDom = document.querySelector('.desc')
+
+        imgUrlList = response.list
+        descDom.innerHTML = `当前页面监测到【${imgUrlList.length}】张图片`
+        markImgDom(response.list);
       }
-    );
-  });
+    }
+  );
+});
+
+// 点击扩展向content_script通讯
+btn.addEventListener("click", async () => {
+  for (const item of imgUrlList) {
+    await delayFn(800);
+    downloadImg(item);
+  }
+
+  // chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+  //   chrome.tabs.sendMessage(
+  //     tabs[0].id,
+  //     { type: "getImg" },
+  //     async function (response) {
+  //       if (response && response.list) {
+  //         markImgDom(response.list);
+
+  //       }
+  //     }
+  //   );
+  // });
 });
 
 // 生成预览图
 function markImgDom(imgUrlList) {
-  const ulDom = document.querySelector('ul')
+  const ulDom = document.querySelector("ul");
   let imgStr = `
     <li>
-      ${imgUrlList.map(e => (
-        `<img src="${e}" alt="">`
-      ))}
+      ${imgUrlList.map((e) => `<img src="${e}" alt="">`)}
     </li>
-  `
-  imgStr = imgStr.replaceAll(',', '')
-  ulDom.innerHTML = imgStr
+  `;
+  imgStr = imgStr.replaceAll(",", "");
+  ulDom.innerHTML = imgStr;
 }
 
 // 下载
@@ -53,7 +71,7 @@ function downloadImg(url) {
 async function delayFn(timer) {
   return new Promise((resolve) => {
     setTimeout(() => {
-      resolve()
+      resolve();
     }, timer);
-  })
+  });
 }
